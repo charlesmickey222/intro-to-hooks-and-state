f28001945cdd2b219baab00d2d050510d2264d26
CharlieMickey <charles.f.mickey@gmail.com>
2023-01-24 11:16:17 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <div className='app-container'>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Gallery.jsx
@@ -0,0 +1,36 @@
+import { sculptureList } from "./data"
+
+const Gallery = () => {
+
+  let index = 0
+
+  function handleClick() {
+    index = index + 1
+  }
+
+  let sculpture = sculptureList[index]
+
+  return (
+    <div className="card">
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+    </div>
+  )
+}
+
+export default Gallery;
